1c421d903143def9088fdd3c724efdd5c4e976e2
cbtc <carlos_ddg@yahoo.es>
2017-04-30 15:13:15 +0200

    fix unmerged files

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -477,12 +477,15 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+<<<<<<< HEAD
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
 extern struct cpufreq_governor cpufreq_gov_barry_allen;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
 extern struct cpufreq_governor cpufreq_gov_lionfish;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_lionfish)
+=======
+>>>>>>> parent of 332373d... Add Barry Allen Governor
 #endif
 
 /*********************************************************************
